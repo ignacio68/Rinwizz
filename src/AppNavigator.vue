ae695fc72583a5fdf26395419de6cfce43b1d17a
Ignacio <buscemi@ya.com>
2019-05-09 13:47:00 +0200

    Using map helpers

--- src/AppNavigator.vue
+++ src/AppNavigator.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
+import { mapState, mapGetters, mapMutations } from 'vuex'
 
 import HomePage from '@pages/HomePage'
 import Welcome from '@pages/Shared/Welcome'
@@ -17,11 +17,9 @@ export default {
   beforeMount() {
     console.log('AppNavigator beforeMount()')
     if (!this.userIsAuthenticated) {
-      // this.$store.commit('navigator/PUSH', Welcome)
       this.PUSH(Welcome)
       console.log('El usuario NO está autenticado')
     } else {
-      // this.$store.commit('navigator/PUSH', HomePage)
       this.PUSH(HomePage)
       console.log('El usuario SI está autenticado')
     }
@@ -33,29 +31,23 @@ export default {
     return {}
   },
   computed: {
-    // ...mapGetters('navigator', ['PAGE_STACK, OPTIONS']),
-
-    pageStack() {
-      return this.$store.getters['navigator/PAGE_STACK']
-      // return this.PAGE_STACK
-    },
-
-    options() {
-      return this.$store.getters['navigator/OPTIONS']
-      // return this.options
-    },
+    ...mapState('user', {
+      user: state => state.user
+    }),
+    ...mapGetters('navigator', { pageStack: 'PAGE_STACK', options: 'OPTIONS' }),
 
     userIsAuthenticated() {
       console.log('Comprobando si el usuario está autenticado')
       return (
-        this.$store.getters['user/user'] !== null &&
-        this.$store.getters['user/user'] !== undefined
+        // this.$store.getters['user/user'] !== null &&
+        // this.$store.getters['user/user'] !== undefined
+        this.user !== null && this.user !== undefined
       )
     }
   },
   methods: {
-    ...mapGetters('navigator', ['PAGE_STACK, OPTIONS']),
     ...mapMutations('navigator', ['PUSH', 'POP']),
+
     storePop() {
       this.$store.commit('navigator/POP')
       // this.POP()
