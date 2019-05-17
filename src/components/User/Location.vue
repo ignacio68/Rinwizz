eb8b51507498d6474832e4f0c9d0ea4c7825a184
Ignacio <ilopez-amor@ya.com>
2019-05-18 01:25:46 +0200

    added comment

--- src/components/User/Location.vue
+++ src/components/User/Location.vue
@@ -66,6 +66,7 @@ export default {
         // url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
         url: 'http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.jpg',
         atribution:
+          // This atribution is when the map owner is stamen.com in terrain mode
           'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
       },
       zoom: 17,
