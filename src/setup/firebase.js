475490ed635ddb6ff9b32bddc4952575929b52b1
Ignacio <buscemi@ya.com>
2019-06-05 12:16:34 +0200

    minor changes

--- src/setup/firebase.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyALVIvKmv5Qbi-_gw411SWrzTbq6IveUiw',
-  authDomain: 'rinwizz-app.firebaseapp.com',
-  databaseURL: 'https://rinwizz-app.firebaseio.com',
-  projectId: 'rinwizz-app',
-  storageBucket: 'rinwizz-app.appspot.com',
-  messagingSenderId: '531370790834'
-}
