555b0077ac010160915e07905f1a479451a892d5
Ignacio <ilopez-amor@ya.com>
2019-07-02 19:49:21 +0200

    updated database

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,32 +1,57 @@
 // import PouchDB from 'pouchdb'
 
 // export const db = new PouchDB('alerts')
+export const sampleUser = {
+  _id: '',
+  type: 'item',
+  name: '',
+  email: '',
+  phone: '',
+  alertPhone: '',
+  link: '',
+  creationDate: '',
+  lastSigninDate: '',
+  isAnonymous: true,
+  isVerified: false,
+  providerId: 2,
+  personal: {
+    age: '',
+    gender: ''
+  },
+  preferences: {
+    entertainments: [],
+    foods: [],
+    sports: []
+  },
+  location: {
+    lat: '',
+    lng: '',
+    address: {}
+  }
+}
 
 export const listAlerts = {
-  "_id": '',
-  "type": 'list',
-  "version": 1,
-  "alerts": {}
-  
+  _id: '',
+  type: 'list',
+  version: 1,
+  alerts: {}
 }
 
 export const sampleAlert = {
-  "_id": '',
-  "type": 'item',
-  "version": 1,
-  "title": '',
-  "text": '',
-  "avatar": '',
-  "altAvatar": '',
-  "startDate": '',
-  "endDate": '',
-  "phone": '',
-  "link": '',
-  "address": {
-    "street": "",
-    "coords": {
-      "lat": "",
-      "lng": ""
-    }
+  _id: '',
+  type: 'item',
+  version: 1,
+  title: '',
+  text: '',
+  avatar: '',
+  altAvatar: '',
+  startDate: '',
+  endDate: '',
+  phone: '',
+  link: '',
+  location: {
+    lat: '',
+    lng: '',
+    address: {}
   }
 }
