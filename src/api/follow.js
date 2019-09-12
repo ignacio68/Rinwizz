7a641ceb92e0b41459ae956cb5a3fe4372fd9876
Ignacio <buscemi@ya.com>
2019-09-12 11:46:57 +0200

    added API

---
+++ src/api/follow.js
@@ -0,0 +1,19 @@
+/**
+ * Get followings
+ * @param usersId { Array } - array con los ids de los followings
+ **/
+export function followingsGet(usersId) {}
+
+/**
+ * Get followers
+ * @param usersId { Array } - array con los ids de los followers
+ **/
+export function followersGet(usersId) {}
+
+export function usersSearch() {}
+
+export function usersShow(usersId, usersName) {}
+
+export function followingCreate(userId, userName) {}
+
+export function followingDestroy(userId, userName) {}
