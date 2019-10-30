e878a0d8dae3423e56a23629f3fdd734e89426ef
Ignacio <buscemi@ya.com>
2019-10-30 12:24:17 +0100

    added storybook

---
+++ src/stories/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+// import MyButton from './Button.vue'
+import AppButton from '../components/AppButton.vue'
+import Hello from '../components/HelloI18n.vue'
+
+export default { title: 'AppButton' }
+
+// export const withText = () => '<my-button>with text</my-button>'
+// export const withemoji = () => '<my-button>😀 😎 👍 💯</my-button>'
+// export const asAComponent = () => ({
+//   components: { MyButton },
+//   template: '<my-button :rounded="true">rounded</my-button>
+// })
+
+export const prueba = () => '<app-button>prueba</app-button>'
+export const hello = () => '<hello></hello>'
