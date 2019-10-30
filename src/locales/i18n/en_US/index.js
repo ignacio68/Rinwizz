f3ce9316e3423428d5298c8601771f7523022553
ignacio68 <ilopez-amor@ya.com>
2019-10-30 09:05:27 +0100

    locale buttons

--- src/locales/i18n/en_US/index.js
+++ src/locales/i18n/en_US/index.js
@@ -1,9 +1,11 @@
 import components from './components'
 import views from './views'
 import errors from './errors'
+import buttons from './buttons'
 
 export default {
   components,
   views,
-  errors
+  errors,
+  buttons
 }
